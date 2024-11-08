25f2a72b45f597ce25c604c5a15f3953c2b2eff5
wbcat <konokaze0@gmail.com>
2024-11-08 17:33:32 +0900

    refine design - add icon, sidebar

--- frontend/src/Main/SideBar.tsx
+++ frontend/src/Main/SideBar.tsx
@@ -15,7 +15,8 @@ import ListItemText from '@mui/material/ListItemText';
 
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import {Link} from "react-router-dom";
+import {Link as RouterLink} from "react-router-dom";
+import { styled } from '@mui/material';
 
 
 
@@ -30,16 +31,20 @@ const SideBar:React.FC =  () => {
         {link: "/Analy", text: "Analy"}
     ]
 
-    
+    const [open, setOpen] = React.useState(true);
 
+    const toggleDrawer = (newOpen: boolean) => () => {
+      setOpen(newOpen);
+    };
     return (
     <div>
   
       {/* <Toolbar /> */}
       
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', p: 0 }}>
+     
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', p: 0 }} role="presentation">
         <IconButton color="primary" aria-label="home">
-        <img src="/logo192.png" alt="Logo" style={{ width: '40px', height: '40px' }} />
+        <img src="/icon.png" alt="Logo" style={{ width: '40px', height: '40px' }} />
         
         </IconButton>
       </Box>
@@ -47,25 +52,45 @@ const SideBar:React.FC =  () => {
       <Divider />
       <List>
         {MenuItem.map((item, index) => (
-          <ListItem key={item.text} disablePadding>
-            <Link to={item.link}>
+          <SideBarLine>
+          <ListItem key={item.text} disablePadding component={RouterLink} to={item.link}>
             <ListItemButton >
               {/* <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon> */}
-              <ListItemText primary={item.text} color="primary"/>
+              <ListItemText primary={item.text} sx={{ color: '#dd4444' }}/>
             </ListItemButton>
-            </Link>
           </ListItem>
+          </SideBarLine>  
         ))}
       </List>
       
      
+       
       
 
     </div>
     )
 };
 
+const SideBarLine = styled(Box)(({ theme }) => ({
+          // Adjust width as needed
+ 
+  backgroundColor: theme.palette.background.paper,
+  position: 'relative',
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    height: '1px',         // Line thickness
+    width: '0%',           // Start with 0% width
+    backgroundColor: theme.palette.primary.main,
+    transition: 'width 0.4s ease-in-out',  // Animation duration and ease
+  },
+  '&:hover::after': {
+    width: '100%',         // Expand to full width on hover
+  },
+}));
 
 export default SideBar;
